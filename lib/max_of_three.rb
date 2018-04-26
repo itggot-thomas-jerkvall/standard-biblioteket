4e6999ce8ae29abcf9a26b4a410da898b28e303e
itggot-thomas-jerkvall <38528645+itggot-thomas-jerkvall@users.noreply.github.com>
2018-04-26 14:56:27 +0200

    gjorde max of three

---
+++ lib/max_of_three.rb
@@ -0,0 +1,22 @@
+# Public: Takes three integers and returns the largest one
+#
+# n1 - one of the integers
+# n2 - one of the integers
+# n3 - one of the integers
+#
+# Examples
+#
+#   max_of_two(5, 10, 1)
+#   # => 10
+#
+# Returns the largest integer
+def max_of_three(n1,n2,n3)
+    max = n1
+    if n1 < n2
+        max = n2
+    end
+    if max < n3
+        max = n3
+    end
+    return max
+end
