e38329423704f92ea742434dd1cde3e3dd09172b
itggot-thomas-jerkvall <38528645+itggot-thomas-jerkvall@users.noreply.github.com>
2018-04-26 14:36:45 +0200

    gjorde max of two

---
+++ lib/max_of_two.rb
@@ -0,0 +1,18 @@
+# Public: Takes two integers and returns the largest one
+#
+# n1 - one of the integers
+# n2 - one of the integers
+#
+# Examples
+#
+#   max_of_two(5, 10)
+#   # => 10
+#
+# Returns the largest number
+def max_of_two(n1, n2)
+    max = n1 
+    if max < n2
+        max = n2
+    end
+    return max
+end
