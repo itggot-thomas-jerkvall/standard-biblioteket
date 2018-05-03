4c991530486bed1d543a8d669bb1161acf9d2378
itggot-thomas-jerkvall <38528645+itggot-thomas-jerkvall@users.noreply.github.com>
2018-05-03 13:19:34 +0200

    gjorde first of

---
+++ lib/first_of.rb
@@ -0,0 +1,13 @@
+# Public: Takes an array and returns the first element of it
+#
+# array - the array
+#
+# Examples
+#
+#   first_of([3,5,6,2])
+#   # => 3
+#
+# Returns the first element of the array
+def first_of(array)
+    return array[0]
+end
