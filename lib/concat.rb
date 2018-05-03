5b63c906873316b3f5a2b1631a28b22af2c968f9
itggot-thomas-jerkvall <38528645+itggot-thomas-jerkvall@users.noreply.github.com>
2018-05-03 13:33:07 +0200

    gjorde concat

---
+++ lib/concat.rb
@@ -0,0 +1,14 @@
+# Public: Takes two arrays adds them 
+#
+# a1 - the first array
+# a2 - the second array
+#
+# Examples
+#
+#   concat([1,2], [3,4])
+#   # => [1,2,3,4]
+#
+# Returns the combined array
+def concat(a1, a2)
+    return a1 + a2
+end
