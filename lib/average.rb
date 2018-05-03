dafe422eaba6bd87a8115ab8b82003342c555678
itggot-thomas-jerkvall <38528645+itggot-thomas-jerkvall@users.noreply.github.com>
2018-05-03 13:58:57 +0200

    gjorde average

---
+++ lib/average.rb
@@ -0,0 +1,19 @@
+# Public: Takes an array and returns the average of the elements
+#
+# array - The array
+#
+# Examples
+#
+#   append([3,3,3,2,4])
+#   # => 3
+#
+# Returns the average of the elements
+def average(array)
+    i = 0
+    output = 0
+    while i < array.length
+        output += array[i]
+        i += 1
+    end
+    return output/(array.length)
+end
